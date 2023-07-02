5c6ae972f1458671343823453f8fd834d29dce0f
kkk <kutimskyi@gmail.com>
2023-07-02 21:14:13 +0300

    second

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,12 +1,13 @@
 import read from './reader';
 import json from './parser';
+import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
   static async load() {
     try {
       const data = await read();
       const value = await json(data);
-      return value;
+      return new GameSaving(JSON.parse(value));
     } catch (err) {
       throw new Error(err);
     }
--- src/js/__tests__/GameSavingLoader.test.js
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -3,20 +3,22 @@ import GameSavingLoader from '../GameSavingLoader';
 jest.setTimeout(15000);
 test('test  GameSavingLoader ', async (done) => {
   const saving = await GameSavingLoader.load();
-  expect(saving).toEqual('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}');
+  expect(saving).toEqual({
+    created: 1546300800,
+    id: 9,
+    userInfo: {
+      id: 1, level: 10, name: 'Hitman', points: 2000,
+    },
+  });
   done();
 });
 
 // test('test catch  GameSavingLoader ', async (done) => {
 //   try{
-//     let GAme = new GameSavingLoader
-//      const saving = await GameSavingLoader.load = (async () => {
-//       // Your code here with await
-//     })();;
-
+//     let json = '';
+//      const saving = await GameSavingLoader.load()
 //   } catch (e){
 //     expect(e.name).toEqual('Oops');
 //   }
-
 //   done();
 // });
